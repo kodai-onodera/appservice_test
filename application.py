92d8baecbdf203a0e2cadfcb8a29bd96f2bb2080
onoderak70 <onoderak70@bbwcfs.local127.0.0.1>
2023-09-15 16:01:51 +0900

    fix

--- application.py
+++ application.py
@@ -3,13 +3,15 @@
 
 import datetime
 from flask import Flask, request
+import socket
 
 app = Flask(__name__)
 
 
 @app.route('/',methods=['GET'])
 def print_totp():
-  client_ip = request.remote_addr
+  # client_ip = request.remote_addr
+  client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
   
   dt = datetime.datetime.now()
   dt_str = dt.strftime("%Y/%m/%d %H:%M:%S")
