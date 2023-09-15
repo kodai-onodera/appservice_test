6675ad55833f3b5828b199416ecbdb9b3ce1632a
onoderak70 <onoderak70@bbwcfs.local127.0.0.1>
2023-09-15 16:51:26 +0900

    print imput data

--- application.py
+++ application.py
@@ -10,15 +10,17 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET'])
 def print_ip():
-  # client_ip = request.remote_addr
   client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-  
   dt = datetime.datetime.now()
   dt_str = dt.strftime("%Y/%m/%d %H:%M:%S")
-  result = dt_str + " \nAccess from " + str(client_ip) + "\n"
-  
+  form = '<form action="/" method="POST"><input name="imput_data"></input><button type="submit">送信</button></form>'
+  result = dt_str + " \nAccess from " + str(client_ip) + "\n" + form + "\n"
   return result
 
+@app.route('/', methods=['POST'])
+def sample_form_temp():
+    result = "Imput value: " + str(request.form['imput_data']) 
+    return result
 
 if __name__ == "__main__":
   app.run()
