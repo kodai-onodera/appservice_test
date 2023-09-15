0d802449697232b5c13806931b86a344c6d7da8a
onoderak70 <onoderak70@bbwcfs.local127.0.0.1>
2023-09-15 16:57:28 +0900

    typo

--- application.py
+++ application.py
@@ -13,13 +13,13 @@ def print_ip():
   client_ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
   dt = datetime.datetime.now()
   dt_str = dt.strftime("%Y/%m/%d %H:%M:%S")
-  form = '<form action="/" method="POST"><input name="imput_data"></input><button type="submit">送信</button></form>'
+  form = '<form action="/" method="POST"><input name="input_data"></input><button type="submit">送信</button></form>'
   result = dt_str + " \nAccess from " + str(client_ip) + "\n" + form + "\n"
   return result
 
 @app.route('/', methods=['POST'])
 def sample_form_temp():
-    result = "Imput value: " + str(request.form['imput_data']) 
+    result = "input value: " + str(request.form['input_data']) 
     return result
 
 if __name__ == "__main__":
